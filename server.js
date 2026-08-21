b83d1b77957e8a4b4d5120110ef2b41266378663
lukazfivee <142459897+lukazfivee@users.noreply.github.com>
2026-08-21 19:05:41 -0300

    Carrega refinamentos finais da V3.1

--- server.js
+++ server.js
@@ -80,6 +80,9 @@ function createApp() {
       if (!html.includes('v3-1-enhancements.css')) {
         html = html.replace('</head>', '  <link rel="stylesheet" href="v3-1-enhancements.css">\n</head>');
       }
+      if (!html.includes('v3-1-refinements.css')) {
+        html = html.replace('</head>', '  <link rel="stylesheet" href="v3-1-refinements.css">\n</head>');
+      }
       if (!html.includes('report-v2.js')) {
         html = html.replace('</body>', '  <script src="report-v2.js"></script>\n</body>');
       }
@@ -88,6 +91,9 @@ function createApp() {
       }
       if (!html.includes('v3-1-enhancements.js')) {
         html = html.replace('</body>', '  <script src="v3-1-enhancements.js"></script>\n</body>');
+      }
+      if (!html.includes('v3-1-refinements.js')) {
+        html = html.replace('</body>', '  <script src="v3-1-refinements.js"></script>\n</body>');
       }
       res.setHeader('Cache-Control', 'no-cache');
       res.type('html').send(html);
